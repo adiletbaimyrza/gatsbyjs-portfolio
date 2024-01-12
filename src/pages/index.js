d86ca9677ddba36fb6c1d19267e65950dbe6735c
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-01-12 19:21:35 +0100

    Add new components and update dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import React from 'react'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
 import LatestArticles from '../components/latestArticles'
+import LatestProjects from '../components/latestProjects'
+import Codepens from '../components/codepens'
+import AboutMe from '../components/aboutMe'
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
+      <AboutMe />
       <LatestArticles />
+      <LatestProjects />
+      <Codepens />
     </Layout>
   )
 }
